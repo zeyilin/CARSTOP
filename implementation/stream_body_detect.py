27f5054b09666b005fcf854bceb5304b9cc59397
Nico Garofano <ngarofano12@gmail.com>
2017-01-23 16:24:10 -0600

    added nonmaximal suppression

--- implementation/stream_body_detect.py
+++ implementation/stream_body_detect.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-
+from imutils.object_detection import non_max_suppression
 
 def draw_detections(img, rects, thickness = 1):
     for x, y, w, h in rects:
@@ -19,9 +19,18 @@ if __name__ == '__main__':
         ret,frame=cap.read()
         if not ret: continue
         # frame = cv2.imread('images/ped0.jpeg')
-        found,w=hog.detectMultiScale(frame, winStride=(8,8), padding=(32,32), scale=1.05)
-        print('Found: {}'.format(len(found)))
-        draw_detections(frame,found)
+        rects,w=hog.detectMultiScale(frame, winStride=(8,8), padding=(32,32), scale=1.05)
+
+        #non-maximal suppression
+        rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+        pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
+        # draw the final bounding boxes
+        for (xA, yA, xB, yB) in pick:
+            cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 0, 255), 2)
+
+        print('Found: {}'.format(len(rects)))
+        draw_detections(frame,rects)
         cv2.imshow('Video', frame)
         if cv2.waitKey(1) & 0xff == ord('q'):
             break
